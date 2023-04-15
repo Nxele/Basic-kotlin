14c670435998ff5fec7617bd097c9b8ec4b8c727
Nxele <sizwenduduzow@gmail.com>
2023-04-15 02:20:25 +0200

    wrote my first method

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,20 @@
 fun main() {
-    println("Hello World!")
+    Person()
+}
+
+fun Person(){
+
+    // Declare and asign a value
+    val name = "Sizwe"
+    val surname = "Nxele"
+    var age = 28
+
+    // Declare only but you need to specify the type:
+    var worth : Int
+    var futureWife : String
+
+    worth = 450000000
+    futureWife = "Sinenhlanhla"
+
+    println(" Name: $name \n Surname: $surname: \n Age: $age \n worth: $worth \n FutureWife: $futureWife")
 }
