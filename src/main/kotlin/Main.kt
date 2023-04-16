66fad8cfdcc7929da93d30f48db0f3f0ae2d8e80
Nxele <sizwenduduzow@gmail.com>
2023-04-16 15:41:33 +0200

    added break & continue for loops

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,9 @@
 fun main() {
     Person()
     DataTypes()
+    StringValues()
+    ifElse()
+    breakContinue()
 }
 
 // Methods & Variables
@@ -48,3 +51,94 @@ fun DataTypes(){
     val num2:Long = num1.toLong()
     print("$num2")
 }
+fun StringValues(){
+
+    // This can also be used to add numbers
+
+    var firstName = "Sizwe"
+    var lastName = "Nxele"
+    println(firstName.plus(lastName))
+}
+
+fun ifElse() {
+    /*Kotlin supports the usual logical conditions from mathematics:
+
+    Less than: a < b
+    Less than or equal to: a <= b
+    Greater than: a > b
+    Greater than or equal to: a >= b
+    Equal to a == b
+    Not Equal to: a != b
+    You can use these conditions to perform different actions for different decisions.
+
+    Kotlin has the following conditionals:
+
+    Use if to specify a block of code to be executed, if a specified condition is true
+    Use else to specify a block of code to be executed, if the same condition is false
+    Use else if to specify a new condition to test, if the first condition is false
+    Use when to specify many alternative blocks of code to be executed*/
+
+    // new if..Else statement
+
+    var age = 28
+
+    var ageGroup = if (age < 23) {
+        "teenage"
+    } else {
+        "Adult"
+    }
+
+    // Or as one statement if it has a single line
+    val time = 20
+    val greeting = if (time < 18) "Good day." else "Good evening."
+
+    println(ageGroup)
+
+
+    // Use can also use WHEN instead if writing if statements
+
+    val day = 7
+    val selectedDay = when (day) {
+        1 -> "Monday"
+        2 -> "Tuesday"
+        2 -> "Wednesday"
+        4 -> "Thursday"
+        5 -> "Friday"
+        6 -> "Saturday"
+        7 -> "Sunday"
+        else -> "Invalid day"
+    }
+    println(" Selected day is : $selectedDay")
+}
+
+fun loops(){
+
+    // while loop
+    var i = 0
+    while (i < 5) {
+        println(i)
+        i++
+    }
+
+    // Do while the code is executed at least once
+    var z = 0
+    do {
+        println(i)
+        i++
+    }
+    while (z < 5)
+}
+fun breakContinue(){
+    var index = 15
+    for(i in 1..index) {
+        if(i==7){
+            continue // skip this loop
+        }
+        if(i==13){
+            break // break the loop when you found what you are loking for
+        }
+        println(i)
+    }
+}
+
+
